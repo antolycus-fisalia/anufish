f9d30d0c6803d1405c06f9a1e69753b24f737a86
Nadya Nur Kamila <nadyanur969@gmail.com>
2026-09-17 13:31:48 +0700

    feat(home): add static home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Beranda</title>
+</head>
+<body>
+    <h1>Selamat Datang di Halaman Beranda PjBL Kami</h1>
+    <p>Ini adalah halaman utama aplikasi transportasi berkelanjutan.</p>
+</body>
+</html>
