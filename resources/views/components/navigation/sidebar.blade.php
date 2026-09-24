7b8d0e330d0b9ef8f5fad2e962ec130dcdff27a2
Istianto Ilham <euphyllia.tomodachi@gmail.com>
2026-09-24 10:53:22 +0700

    fix: logout button in sidebar

--- resources/views/components/navigation/sidebar.blade.php
+++ resources/views/components/navigation/sidebar.blade.php
@@ -6,55 +6,47 @@
         bg-white
         p-5
         lg:block
-    "
->
+    ">
     <div class="mb-8 text-2xl font-bold text-anufish-blue">
         Anufish
     </div>
 
     <nav class="space-y-1">
 
-        <a
-            href="#"
-            class="block rounded-xl px-4 py-3 font-semibold text-slate-500 hover:bg-cyan-50 hover:text-anufish-blue"
-        >
+        <a href="#"
+            class="block rounded-xl px-4 py-3 font-semibold text-slate-500 hover:bg-cyan-50 hover:text-anufish-blue">
             Dashboard
         </a>
 
-        <a
-            href="#"
-            class="block rounded-xl px-4 py-3 font-semibold text-slate-500 hover:bg-cyan-50 hover:text-anufish-blue"
-        >
+        <a href="#"
+            class="block rounded-xl px-4 py-3 font-semibold text-slate-500 hover:bg-cyan-50 hover:text-anufish-blue">
             Scan Ikan
         </a>
 
-        <a
-            href="#"
-            class="block rounded-xl px-4 py-3 font-semibold text-slate-500 hover:bg-cyan-50 hover:text-anufish-blue"
-        >
+        <a href="#"
+            class="block rounded-xl px-4 py-3 font-semibold text-slate-500 hover:bg-cyan-50 hover:text-anufish-blue">
             Riwayat Scan
         </a>
 
-        <a
-            href="#"
-            class="block rounded-xl px-4 py-3 font-semibold text-slate-500 hover:bg-cyan-50 hover:text-anufish-blue"
-        >
+        <a href="#"
+            class="block rounded-xl px-4 py-3 font-semibold text-slate-500 hover:bg-cyan-50 hover:text-anufish-blue">
             Artikel Saya
         </a>
 
-        <a
-            href="#"
-            class="block rounded-xl bg-cyan-50 px-4 py-3 font-semibold text-anufish-blue"
-        >
+        <a href="#" class="block rounded-xl bg-cyan-50 px-4 py-3 font-semibold text-anufish-blue">
             Profil
         </a>
 
-        <a
-            href="#"
-            class="mt-5 block rounded-xl px-4 py-3 font-semibold text-red-600 hover:bg-red-50"
-        >
+        <a href="{{ route('logout') }}"
+            onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+            class="mt-5 block rounded-xl px-4 py-3 font-semibold text-red-600 hover:bg-red-50">
             Logout
         </a>
+
+        <!-- Form Tersembunyi yang akan dikirim via POST -->
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+            @csrf
+        </form>
 
     </nav>
 </aside>
